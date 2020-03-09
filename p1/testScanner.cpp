f0315bdd6f325cf68aa1f0539ae7f473a17cd465
zegster <dngo1996@gmail.com>
2020-03-09 05:06:41 -0500

    got EOF recognition working

--- p1/testScanner.cpp
+++ p1/testScanner.cpp
@@ -17,10 +17,10 @@ int testScanner(string file_name)
 	
 	//Check if teh file is open and associated with the stream object
 	ifstream file(file_name.c_str());
+	unsigned int current_line = 1;  //Keep track of the current line number of the file
+	Token token;                    //Token holder for displaying purpose (can be useful later in the future...)
 	if(file.is_open()) {
-		unsigned int current_line = 1;
 		string input;
-		Token token;
 		while(getline(file, input)) {
 			//Filter input by removing comments and whitespace
 
@@ -38,6 +38,20 @@ int testScanner(string file_name)
 	else {
 		cout << "[ERROR] Can't open file!" << endl;
 		return -1;
+	}
+
+	//Once all lines of the files has been traverse, print the EOF token
+	if(file.eof()) {
+		//Check if the current line is the initial line. If not, reduce by 1
+		if(current_line > 1) {
+			current_line--;
+		}
+
+		//Display EOF token
+		token.line_number = current_line;
+		token.id = eofTk;
+		token.value = "EOF";
+		tokenToString(token);
 	}
 
 	file.close();
