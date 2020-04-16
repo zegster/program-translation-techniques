e3e61ba091ad4f9177f66491e0660626f4413c4d
zegster <dngo1996@gmail.com>
2020-04-16 04:55:26 -0500

    update bnf

--- p2/parser.cpp
+++ p2/parser.cpp
@@ -41,7 +41,6 @@ NodeT *Parser::program()
 	node->c1 = vars();
 	node->c2 = block();
 	
-	cout << "done program" << endl;
 	return node;
 }
 
@@ -62,7 +61,7 @@ NodeT *Parser::block()
 		nextScan();
 
 		node->c1 = vars();
-		//node->c2 = stats();
+		node->c2 = stats();
 
 		//Check if token is [rightCurlyTk]
 		if((tk.id == opTk) && (operator_map[tk.value] == "rightCurlyTk")) {
@@ -115,7 +114,6 @@ NodeT *Parser::vars()
 						nextScan();
 						
 						node->c1 = vars();
-
 						return node;
 					} else {
 						expected_token.assign("semiColonTk");
@@ -144,12 +142,461 @@ NodeT *Parser::vars()
 }
 
 
+/* ====================================================================================================
+* Function    :  expr()
+* Definition  :  <expr> -> <N> - <expr> | <N>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::expr()
+{
+	//Create the node <expr>
+	NodeT *node = createNode("<expr>");
+
+	node->c1 = N();
+	
+	//Check if token is [minusTk]
+	if((tk.id == opTk) && (operator_map[tk.value] == "minusTk")) {
+		node->tokens.push_back(tk);  //Store the [minus] in this node
+		nextScan();
+
+		node->c2 = expr();
+	}
+
+	return node;
+}
 
 
+/* ====================================================================================================
+* Function    :  N()
+* Definition  :  <N> -> <A> / <N> | <A> * <N> | <A> 
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::N()
+{
+	//Create the node <N>
+	NodeT *node = createNode("<N>");
+
+	node->c1 = A();
+
+	//Check if token is [opTk]
+	if(tk.id == opTk) {
+		//Check if token is [divideTk]
+		if(operator_map[tk.value] == "divideTk") {
+			node->tokens.push_back(tk);  //Store the [divide] in this node
+			nextScan();
+
+			node->c2 = N();
+		}
+		//Check if token is [multiplyTk]
+		else if(operator_map[tk.value] == "multiplyTk") {
+			node->tokens.push_back(tk);  //Store the [multiply] in this node
+			nextScan();
+
+			node->c2 = N();
+		}
+	}
+
+	return node;
+}
 
 
+/* ====================================================================================================
+* Function    :  A()
+* Definition  :  <A> -> <M> + <A> | <M>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::A()
+{
+	//Create the node <A>
+	NodeT *node = createNode("<A>");
+
+	node->c1 = M();
+	
+	//Check if token is [plusTk]
+	if((tk.id == opTk) && (operator_map[tk.value] == "plusTk")) {
+		node->tokens.push_back(tk);  //Store the [plus] in this node
+		nextScan();
+		
+		node->c2 = A();
+	}	
+
+	return node;
+}
 
 
+/* ====================================================================================================
+* Function    :  M()
+* Definition  :  <M> -> * <M> | <R>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::M()
+{
+	//Create the node <M>
+	NodeT *node = createNode("<M>");
+
+	//Check if token is [multiplyTk]
+	if((tk.id == opTk) && (operator_map[tk.value] == "multiplyTk")) {
+		node->tokens.push_back(tk);
+		nextScan();
+
+		node->c1 = M();
+		return node;
+	}
+
+	node->c1 = R();
+	return node;
+}
+
+
+/* ====================================================================================================
+* Function    :  R()
+* Definition  :  <R> -> ( <expr> ) | Identifier | Integer
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::R()
+{
+	//Create the node <R>
+	NodeT *node = createNode("<R>");
+
+	//Check if token is [leftParenTk]
+	if((tk.id == opTk) && (operator_map[tk.value] == "leftParenTk")) {
+		nextScan();
+
+		node->c1 = expr();
+
+		//Check if token is [rightParenTk]
+		if((tk.id == opTk) && (operator_map[tk.value] == "rightParenTk")) {
+			nextScan();
+			return node;
+		} else {
+			expected_token.assign("rightParenTk");
+			parserError();
+			exit(EXIT_FAILURE);
+		}
+	}
+	//Check if token is [idTk]
+	else if(tk.id == idTk) {
+		node->tokens.push_back(tk);  //Store the [identifier] in this node
+		nextScan();
+		return node;
+	}
+	//Check if token is [intTk]
+	else if(tk.id == intTk) {
+		node->tokens.push_back(tk);  //Store the [integer] in this node
+		nextScan();
+		return node;
+	}
+	else {
+		expected_token.assign("leftParenTk or idkTk or intTk");
+		parserError();
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+/* ====================================================================================================
+* Function    :  stats()
+* Definition  :  <stats> -> <stat> <mStat>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::stats()
+{
+	//Create the node <stats>
+	NodeT *node = createNode("<stats>");
+
+	node->c1 = stat();
+	node->c2 = mStat();
+
+	return node;
+}
+
+
+/* ====================================================================================================
+* Function    :  mStat()
+* Definition  :  <mStat> -> empty | <stat> <mStat>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::mStat()
+{
+	//Create the node <mStat>
+	NodeT *node = createNode("<mStat>");
+
+	//Check if token is [inTk], [outTk], [iffyTk], [loopTk], [labelTk], [gotoTk], [idTk] 
+	if( 
+		((tk.id == keywordTk) && ( 
+			(keyword_map[tk.value] == "inTk") ||
+			(keyword_map[tk.value] == "outTk") ||
+			(keyword_map[tk.value] == "iffyTk") ||
+			(keyword_map[tk.value] == "loopTk") ||
+			(keyword_map[tk.value] == "labelTk") ||
+			(keyword_map[tk.value] == "gotoTk")
+		)) || (tk.id == idTk)
+	) {
+		node->c1 = stat();
+		node->c2 = mStat();
+		return node;
+	} else {
+		node->tokens.push_back(EMPTY_TK);
+		return node;
+	}
+}
+
+
+/* ====================================================================================================
+* Function    :  stat()
+* Definition  :  <stat> -> <in> ; | <out> ; | <block> | <if> ; | <loop> ; | <assign> ; | <goto> ; | 
+                  <label> ; 
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::stat()
+{
+	//Create the node <stat>
+	NodeT *node = createNode("<stat>");
+
+	//Check if token is [keywordTk]
+	if(tk.id == keywordTk) {
+		//Check if token is [inTk]
+		if(keyword_map[tk.value] == "inTk") {
+			nextScan();
+
+			node->c1 = in();
+
+			//Check if token is [semiColonTk]
+			return node;
+		}
+		//Check if token is [outTk]
+		else if(keyword_map[tk.value] == "outTk") {
+			nextScan();
+
+			node->c1 = out();
+
+			//Check if token is [semiColonTk]
+			return node;
+		}
+		//Check if token is [iffyTk]
+		else if(keyword_map[tk.value] == "iffyTk") {
+			nextScan();
+
+			node->c1 = iffy();
+
+			//Check if token is [semiColonTk]
+			return node;
+		}
+		//Check if token is [loopTk]
+		else if(keyword_map[tk.value] == "loopTk") {
+			nextScan();
+
+			node->c1 = loop();
+
+			//Check if token is [semiColonTk]
+			return node;
+		}
+		//Check if token is [labelTk]
+		else if(keyword_map[tk.value] == "labelTk") {
+			nextScan();
+
+			node->c1 = label();
+
+			//Check if token is [semiColonTk]
+			return node;
+		}
+		//Check if token is [gotoTk]
+		else if(keyword_map[tk.value] == "gotoTk") {
+			nextScan();
+
+			node->c1 = goTo();
+
+			//Check if token is [semiColonTk]
+			return node;
+		} else {
+			expected_token.assign("in or out or iffy or loop or label or goto or identifier");
+			parserError();
+			exit(EXIT_FAILURE);
+		}
+	}
+	//Check if token is [idTk]
+	else if(tk.id == idTk) {
+		/*NOTE: the scanner retrieves the next token before assign is called. 
+		The identifer token will be lost if it not temporarily stored. */
+		Token temp_tk = tk;
+		nextScan();
+
+		node->c1 = assign();
+		node->c1->tokens.push_back(temp_tk);
+		return node;
+	} else {
+		expected_token.assign("in or out or iffy or loop or label or goto or identifier");
+		parserError();
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+/* ====================================================================================================
+* Function    :  in()
+* Definition  :  <in> -> in Identifier
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::in()
+{
+	//Create the node <in>
+	NodeT *node = createNode("<in>");
+
+	//Check if token is [idTk]
+	if(tk.id == idTk) {
+		node->tokens.push_back(tk);
+		nextScan();
+		return node;
+	} else {
+		expected_token.assign("idTk");
+		parserError();
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+/* ====================================================================================================
+* Function    :  out()
+* Definition  :  <out> -> out <expr>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::out()
+{
+	//Create the node <out>
+	NodeT *node = createNode("<out>");
+	nextScan();
+
+	node->c1 = expr();
+	return node;
+}
+
+
+/* ====================================================================================================
+* Function    :  iffy()
+* Definition  :  <iffy> -> iffy [ <expr> <RO> <expr> ] then <stat>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::iffy()
+{
+	//Create the node <iffy>
+	NodeT *node = createNode("<iffy>");
+
+	//Check if token is [leftBracketTk]
+	if((tk.id == opTk) && (operator_map[tk.value] == "leftBracketTk")) {
+		nextScan();
+		
+		node->c1 = expr();
+		node->c2 = RO();
+		node->c3 = expr();
+		
+		//Check if token is [rightBracketTk]
+		if((tk.id == opTk) && (operator_map[tk.value] == "rightBracketTk")) {
+			nextScan();
+
+			//Check if token is [thenTK]
+			if((tk.id == keywordTk) && (keyword_map[tk.value] == "thenTk")) {
+				nextScan();
+
+				node->c4 = stat();
+				return node;
+			} else {
+				expected_token.assign("thenTk");
+				parserError();
+				exit(EXIT_FAILURE);
+			}
+		} else {
+			expected_token.assign("rightBracketTk");
+			parserError();
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		expected_token.assign("leftBracketTk");
+		parserError();
+		exit(EXIT_FAILURE);
+		
+	}
+}
+
+
+/* ====================================================================================================
+* Function    :  loop()
+* Definition  :  <loop> -> loop [ <expr> <RO> <expr> ] <stat>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::loop()
+{
+	//Create the node <loop>
+	NodeT *node = createNode("<loop>");
+	return node;
+}
+
+
+/* ====================================================================================================
+* Function    :  assign()
+* Definition  :  <assign> -> Identifier := <expr>
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::assign()
+{
+	//Create the node <assign>
+	NodeT *node = createNode("<assign>");
+	return node;
+}
+
+
+/* ====================================================================================================
+* Function    :  label()
+* Definition  :  <label> -> label Identifier
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::label()
+{
+	//Create the node <label>
+	NodeT *node = createNode("<label>");
+	return node;
+}
+
+
+/* ====================================================================================================
+* Function    :  goTo()
+* Definition  :  <goto> -> goto Identifier
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::goTo()
+{
+	//Create the node <goTo>
+	NodeT *node = createNode("<goTo>");
+	return node;
+}
+
+
+/* ====================================================================================================
+* Function    :  RO()
+* Definition  :  <RO> -> < | < < (two tokens) | > | > > (two tokens) | == (one token ==) | < > (two tokens) 
+* Parameter   :  none. 
+* Return      :  individual NodeT.
+==================================================================================================== */
+NodeT *Parser::RO()
+{
+	//Create the node <RO>
+	NodeT *node = createNode("<RO>");
+	return node;
+}
 
 
 /* ====================================================================================================
@@ -206,14 +653,15 @@ void Parser::parserError()
 {
 	cout << "[ERROR] parsing error at " << scanner.getScannerPosition() << " -> expected [" 
 		<< expected_token << "] but received [" << tk.value << "]" << endl;
+	file.close();
 }
 
 
 /* ====================================================================================================
 * Function    :  parse()
-* Definition  :  .
-* Parameter   :  .
-* Return      :  .
+* Definition  :  constructing the parse tree from the start symbol.
+* Parameter   :  none.
+* Return      :  root node.
 ==================================================================================================== */
 NodeT *Parser::parse()
 {
@@ -229,6 +677,7 @@ NodeT *Parser::parse()
 		exit(EXIT_FAILURE);
 	} else {
 		cout << "[INFO] parsing complete with no error... continue to the next step." << endl;
+		file.close();
 		return root; 
 	}
 }
